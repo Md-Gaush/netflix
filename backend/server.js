25ca0d1cf30844fad66a6d38a34d491e501533f6
Md Gaush <mdgaush15130@gmail.com>
2025-10-31 23:35:43 +0530

    fixed route

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api",route)
 
 
 app.use(express.static(path.join(_dirname,"/frontend/dist")))
-app.get('/*',(req,res)=>{
+app.get(/.*/, (req, res) => {
   res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
 })
 
